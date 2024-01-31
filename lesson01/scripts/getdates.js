cbb2cc202f1017e12170679b966c60398a61a7e0
yanocuangana <yanocuanganagmail.com>
2024-01-31 14:11:08 +0100

    add

--- lesson01/scripts/getdates.js
+++ lesson01/scripts/getdates.js
@@ -1,3 +1,15 @@
 
 document.getElementById("lastModified").innerHTML= new Date(document.lastModified);
 document.getElementById("year").innerHTML = new Date().getFullYear();
+
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "favicon/close_white_36dp.svg";
+    }
+    else{
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "favicon/close_white_36dp.svg";
+    }
+}
