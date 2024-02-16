c5a01b3f391330eb0dd7bafc9cba516727041ec5
yanocuangana <yanocuanganagmail.com>
2024-02-16 15:52:51 +0100

    add

--- lesson04/chamber/scripts/getdates.js
+++ lesson04/chamber/scripts/getdates.js
@@ -1,5 +1,5 @@
 const currentDateSpan = document.querySelector("#lastModified");
-
+document.getElementById("year").innerHTML = new Date().getFullYear();
 const now = new Date();
 currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
   dateStyle: "full",
