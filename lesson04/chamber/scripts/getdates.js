7b51d009d361a1568ac659070ea542ba02bf5d40
yanocuangana <yanocuanganagmail.com>
2024-02-17 21:34:42 +0100

    add

--- lesson04/chamber/scripts/getdates.js
+++ lesson04/chamber/scripts/getdates.js
@@ -4,3 +4,15 @@ const now = new Date();
 currentDateSpan.textContent = new Intl.DateTimeFormat("en-GB", {
   dateStyle: "full",
 }).format(now);
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "favicon/close_white_36dp.svg";
+  }
+  else{
+      menuMobile.classList.add('open')
+      document.querySelector('.icon').src = "favicon/close_white_36dp.svg";
+  }
+}
